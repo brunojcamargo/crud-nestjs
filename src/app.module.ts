130064b4372e6d48a1554eaa629d492a1f55c8bb
Bruno Camargo <brunojcamargo1@outlook.com>
2024-01-13 23:53:11 -0300

    feat: add docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CarModule } from './car/car.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://root:toor@localhost:27017/crud?authSource=admin'), CarModule],
+  imports: [MongooseModule.forRoot('mongodb://root:toor@mongo:27017/crud?authSource=admin'), CarModule],
   controllers: [AppController],
   providers: [AppService],
 })
