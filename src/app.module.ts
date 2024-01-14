7b9241de5e521ff806537b7c204829358cfa9f0a
Bruno Camargo <brunojcamargo1@outlook.com>
2024-01-13 22:53:04 -0300

    feat: add mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://root:toor@localhost:27017/crud?authSource=admin')],
   controllers: [AppController],
   providers: [AppService],
 })
