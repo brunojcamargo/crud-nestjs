581a48b456d94f9705c6d279ba1c430a0c7e906a
Bruno Camargo <brunojcamargo1@outlook.com>
2024-01-13 22:54:24 -0300

    feat: add car resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CarModule } from './car/car.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://root:toor@localhost:27017/crud?authSource=admin')],
+  imports: [MongooseModule.forRoot('mongodb://root:toor@localhost:27017/crud?authSource=admin'), CarModule],
   controllers: [AppController],
   providers: [AppService],
 })
